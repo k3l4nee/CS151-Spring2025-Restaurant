c158e3589640a24cb7a6756d93a10d3a59dd4322
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-21 14:20:44 -0700

    add getters, setters, consturctor

--- PartTimeEmployee.java
+++ PartTimeEmployee.java
@@ -5,24 +5,45 @@ public class PartTimeEmployee extends Employee {
     private double hourlyWage;
     private double hoursWorked;
 
-    public PartTimeEmployee(String name, String jobTitle, double hourlyWage, double hoursWorked, String phoneNumber, String email) {
+    public PartTimeEmployee(String name, String jobTitle, double hourlyWage, double hoursWorked, String phoneNumber,
+            String email) {
         super(name, phoneNumber, email);
         this.jobTitle = "Part-time " + jobTitle;
         this.hourlyWage = hourlyWage;
         this.hoursWorked = hoursWorked;
     }
 
-    public static void showPartTimeOptions(String response, Scanner sc){
+    public static void showPartTimeOptions(String response, Scanner sc) {
         
     }
 
+    // getters
     @Override
-    public String getJobTitle(){
+    public String getJobTitle() {
         return this.jobTitle;
     }
+
     @Override
-    public double getPay(){
+    public double getPay() {
         return this.hourlyWage * this.hoursWorked;
     }
-}
 
+    public double getHoursWorked() {
+        return this.hoursWorked;
+    }
+
+    // setters
+    @Override
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = "Part-time " + jobTitle;
+    }
+
+    @Override
+    public void setPay(double hourlyWage) {
+        this.hourlyWage = hourlyWage;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+}
