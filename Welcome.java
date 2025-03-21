615a03d19c8a20c867c838781b7abf142a7378bb
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-20 18:27:19 -0700

    rework welcome, change options

--- Welcome.java
+++ Welcome.java
@@ -5,26 +5,32 @@ public class Welcome {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to House Zhong!");
         System.out.println("-----------------------");
+        System.out.println("Are you an employee or customer?\n");
         System.out.println(
-                "Enter '1' if you're a staff member \nEnter '2' if you're a customer\nEnter 'Exit' to leave");
+                "  1. Employee\n  2. Customer\n  3. Exit\n");
+        System.out.println("Pick an option (1-3):");
+
         String response = sc.next();
 
         while (true) {
             if (response.equals("1")) {
-                Employee employee = new Employee();
-                employee.showEmployeeOptions(response, sc);
+                Employee.showEmployeeOptions(response, sc);
                 break;
             } else if (response.equals("2")) {
-                Customer customer = new Customer();
-                customer.showCustomerOptions(response, sc);
+                Customer.showCustomerOptions(response, sc);
+                break;
+            } else if (response.equals("3")) {
+                System.out.println("Goodbye!");
                 break;
             } else if (response.equalsIgnoreCase("Exit")) {
                 System.out.println("Goodbye!");
                 break;
             } else {
                 System.out.printf("\n'%s' is not an available option. Try again.\n", response);
+                System.out.println("Are you an employee or customer?\n");
                 System.out.println(
-                        "Enter '1' if you're a staff member \nEnter '2' if you're a customer\nEnter 'Exit' to leave");
+                        "  1. Employee\n  2. Customer\n  3. Exit\n");
+                System.out.println("Pick an option (1-3):");
                 response = sc.next();
             }
         }
