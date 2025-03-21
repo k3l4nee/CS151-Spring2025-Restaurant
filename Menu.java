cba37a26fb1cb357fbc365f8666032b42d5ec815
Tina <tinathalai@gmail.com>
2025-03-21 11:58:16 -0700

    test

--- Menu.java
+++ Menu.java
@@ -1,4 +1,23 @@
 public class Menu {
+    private String name;
+    private int id;
+    private String description;
+    private String category;
+    private double price;
+    private boolean available;
+    private String ingredients;
+
+    public void listBestSellers() {
+
+    }
+
+    public void listVegetarianOptions() {
+
+    }
+
+    public void listAllergens() {
+        
+    }
 
     public static void main(String[] args) {
         System.out.println("menu");
