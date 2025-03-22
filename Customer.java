2bb669cbdf7f636be7379304920d0c3aa81e3ea7
Tina <tinathalai@gmail.com>
2025-03-22 00:35:24 -0700

    update methods

--- Customer.java
+++ Customer.java
@@ -1,8 +1,46 @@
-import java.util.Scanner;
-
 public class Customer {
+    private String type;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private int loyaltyPoints;
+    //private Table currentTable;
+    //private ArrayList<Order> orderHistory;
+
+    // constructor
+    public Customer(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = (phoneNumber != null ? phoneNumber : "N/A");
+        this.email = (email != null ? email : "N/A");
+    }
     
-    public void showCustomerOptions(String response, Scanner in) {
-        System.out.println("customer options");
+    // getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String email() {
+        return this.email;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void updateInfo(String name, String phoneNumber, String email) {
+
     }
 }
