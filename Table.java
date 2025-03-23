1d18c36fdf1ebc64f0b61fa77f28b8628778d99a
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-23 01:49:53 -0700

    need to fill in methods

--- Table.java
+++ Table.java
@@ -1,4 +1,29 @@
+import java.util.List;
 
 public class Table implements Reservable {
+
+    @Override
+    public boolean makeReservation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeReservation'");
+    }
+
+    @Override
+    public boolean cancelReservation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cancelReservation'");
+    }
+
+    @Override
+    public boolean isAvailable() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAvailable'");
+    }
+
+    @Override
+    public List<Object> getAvailableTimeSlots() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAvailableTimeSlots'");
+    }
     
 }
