5baf7bfdbcaf34c571a0c41fa5012ce543c08d8d
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-22 23:13:44 -0700

    adding House Zhong as default restaurant

--- Welcome.java
+++ Welcome.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Welcome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Restaurant restaurant = new Restaurant("House Zhong", "1 Washington Sq, San Jose, CA 95192", "10am to 10pm", "804-737-1111", 150);
         Options.showWelcomeOptions(sc);
         sc.close();
     }
