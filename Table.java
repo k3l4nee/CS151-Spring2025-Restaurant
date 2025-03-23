9d17e3b90f1f732bc095853953c00de63da58a42
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-23 13:31:00 -0700

    table

--- Table.java
+++ Table.java
@@ -1,29 +1,3 @@
-import java.util.List;
-
 public class Table implements Reservable {
 
-    @Override
-    public boolean makeReservation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeReservation'");
-    }
-
-    @Override
-    public boolean cancelReservation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cancelReservation'");
-    }
-
-    @Override
-    public boolean isAvailable() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAvailable'");
-    }
-
-    @Override
-    public List<Object> getAvailableTimeSlots() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAvailableTimeSlots'");
-    }
-    
 }
