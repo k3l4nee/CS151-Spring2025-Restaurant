a3c880d0da8e2a33e2440eb693dc9f1a2b7b6912
Tina <tinathalai@gmail.com>
2025-03-22 18:31:38 -0700

    added isAvailable, isVegetarian, findAllergens, listAllergens

---
+++ MenuItem.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MenuItem {
+    private String name;
+    private String category;
+    private double price;
+    private int id;
+    private String description;
+    private List<String> ingredients;
+    private int stock;
+    private static final List<String> COMMON_FOOD_ALLERGIES = List.of("Peanuts", "Tree Nuts", "Dairy", "Soy", "Eggs", "Fish", "Shellfish", "Wheat");
+    private static final List<String> NOT_VEGETARIAN_FRIENDLY = List.of("Chicken", "Beef", "Pork",  "Turkey", "Fish", "Shrimp");
+
+    // constructor
+    public MenuItem(String name, double price, int stock, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.stock = stock;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setIngredients(List<String> ingredients) { 
+        this.ingredients = ingredients;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    // methods
+    public boolean isAvailable(int stock) {
+        if(stock > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isVegetarian() {
+        List<String> itemIngredients = getIngredients();
+        for(String ingredient : itemIngredients) {
+            if(NOT_VEGETARIAN_FRIENDLY.contains(ingredient)) {
+                return false; //menu item is not vegetarian
+            }
+        }
+        return true;
+    }
+
+    public List<String> findAllergens() {
+        List<String> itemIngredients = getIngredients();
+        List<String> allergensList = new ArrayList<>();
+        for(String ingredient : itemIngredients) {
+            if(COMMON_FOOD_ALLERGIES.contains(ingredient)) {
+                allergensList.add(ingredient); //menu item contains allergen(s)
+            }
+        }
+        return allergensList;
+    }
+
+    public void listAllergens() {
+        List<String> allergensList = findAllergens();
+        if(allergensList.isEmpty()) {
+            System.out.println("N/A");
+        } else {
+            System.out.println("Contains: " + allergensList);
+        }
+    }
+
+    public void updatePrice() {
+
+    }
+
+    public boolean checkIngredientsAvailability() {
+        return true;
+    }
+    
+    public boolean changeAvailability() {
+        return true;
+    }
+}
