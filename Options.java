7fd19ceb29a85880e17ffd8ba01db79dead33082
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-23 13:21:26 -0700

    added full time and restaurant admin options

--- Options.java
+++ Options.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Options {
 
     // welcome options
-    public static void showWelcomeOptions(Scanner sc) {
+    public static void showWelcomeOptions(Scanner sc, Restaurant restaurant) {
         System.out.println("-----------------------");
         System.out.println("Are you an employee or customer?\n");
         System.out.println(
@@ -14,10 +14,10 @@ public class Options {
 
         while (true) {
             if (response.equals("1")) {
-                Options.showEmployeeOptions(response, sc);
+                Options.showEmployeeOptions(sc, restaurant);
                 break;
             } else if (response.equals("2")) {
-                Options.showCustomerOptions(response, sc);
+                Options.showCustomerOptions(sc);
                 break;
             } else if (response.equals("3") || response.equalsIgnoreCase("Exit")) {
                 System.out.println("Goodbye!");
@@ -34,28 +34,29 @@ public class Options {
     }
 
     // employee options
-    public static void showEmployeeOptions(String response, Scanner sc) {
+    public static void showEmployeeOptions(Scanner sc, Restaurant restaurant) {
         System.out.println("-----------------------");
         System.out.println("Are you a full-time or part-time employee?\n");
         System.out.println(
                 "  1. Full-time\n  2. Part-time\n  3. Back\n  4. Exit\n");
         System.out.println("Pick an option (1-4):");
 
-        response = sc.next();
+        String response = sc.next();
 
         while (true) {
             if (response.equals("1")) {
-                Options.showFullTimeOptions(response, sc);
+                Options.showFullTimeOptions(sc, restaurant);
                 break;
             } else if (response.equals("2")) {
-                Options.showPartTimeOptions(response, sc);
+                Options.showPartTimeOptions(sc);
                 break;
             } else if (response.equals("3")) {
-                Options.showWelcomeOptions(sc);
+                Options.showWelcomeOptions(sc, restaurant);
                 break;
             } else if (response.equals("4") || response.equalsIgnoreCase("Exit")) {
                 System.out.println("Goodbye!");
-                break;
+                sc.close();
+                System.exit(0);
             } else {
                 System.out.printf("\n'%s' is not an available option. Try again.\n", response);
                 System.out.println("Are you a full-time or part-time employee?\n");
@@ -68,33 +69,34 @@ public class Options {
     }
 
     // full-time employee options
-    public static void showFullTimeOptions(String response, Scanner sc) {
+    public static void showFullTimeOptions(Scanner sc, Restaurant restaurant) {
         System.out.println("-----------------------");
-        System.out.println("Full-time employee options\n");
+        System.out.println("Full-time Employee Options\n");
         System.out.println(
                 "  1. Restaurant Admin\n  2. Inventory\n  3. Back\n  4. Exit\n");
         System.out.println("Pick an option (1-4):");
 
-        response = sc.next();
+        String response = sc.next();
 
         while (true) {
             if (response.equals("1")) {
-                Options.showRestaurantAdminOptions(response, sc);
+                Options.showRestaurantAdminOptions(sc, restaurant);
                 break;
             } else if (response.equals("2")) {
-                Options.showInventoryOptions(response, sc);
+                Options.showInventoryOptions(sc);
                 break;
             } else if (response.equals("3")) {
-                Options.showEmployeeOptions(response, sc);
+                Options.showEmployeeOptions(sc, restaurant);
                 break;
             } else if (response.equals("4") || response.equalsIgnoreCase("Exit")) {
                 System.out.println("Goodbye!");
-                break;
+                sc.close();
+                System.exit(0);
             } else {
                 System.out.printf("\n'%s' is not an available option. Try again.\n", response);
-                System.out.println("Are you a full-time or part-time employee?\n");
+                System.out.println("Full-time Employee Options\n");
                 System.out.println(
-                        "  1. Full-time\n  2. Part-time\n  3. Back\n  4. Exit\n");
+                        "  1. Restaurant Admin\n  2. Inventory\n  3. Back\n  4. Exit\n");
                 System.out.println("Pick an option (1-4):");
                 response = sc.next();
             }
@@ -102,22 +104,161 @@ public class Options {
     }
 
     // part-time employee options
-    public static void showPartTimeOptions(String response, Scanner sc) {
+    public static void showPartTimeOptions(Scanner sc) {
 
     }
 
     // customer options
-    public static void showCustomerOptions(String response, Scanner sc) {
+    public static void showCustomerOptions(Scanner sc) {
 
     }
 
     // restaurant admin options
-    public static void showRestaurantAdminOptions(String response, Scanner sc) {
+    public static void showRestaurantAdminOptions(Scanner sc, Restaurant restaurant) {
+        System.out.println("-----------------------");
+        System.out.println("Restaurant Admin Options\n");
+        System.out.println(
+                "  1. Hire Full-Time Employee\n  2. Hire Part-Time Employee\n  3. Fire Employee\n  4. Get All Employee Details\n  5. Back\n  6. Exit\n");
+        System.out.println("Pick an option (1-6):");
 
+        String response = sc.next();
+
+        while (true) {
+            if (response.equals("1")) {
+                System.out.println("Name?");
+                String name = sc.next();
+
+                System.out.println("Job title? 'Full-time' [enter]");
+                String jobTitle = sc.next();
+
+                double hourlyWage = 0;
+                while (true) {
+                    try {
+                        System.out.println("Salary?");
+                        hourlyWage = sc.nextDouble();
+
+                        if (hourlyWage <= 0) {
+                            throw new InvalidPayException("Salary must be greater than 0. Please try again.");
+                        }
+                        break;
+                    } catch (InvalidPayException e) {
+                        System.out.println("Error: " + e.getMessage());
+                    } catch (Exception e) {
+                        System.out.println("Error: Enter a numeric value.");
+                        sc.next();
+                    }
+                }
+
+                System.out.println("Phone number?");
+                String phoneNumber = sc.next();
+
+                System.out.println("Email?");
+                String email = sc.next();
+
+                Employee newFullTimeEmployee = new FullTimeEmployee(name, jobTitle, hourlyWage, phoneNumber, email);
+
+                restaurant.addEmployee(newFullTimeEmployee);
+                System.out.println("Added " + newFullTimeEmployee.getName() + " to " + restaurant.getName());
+                System.out.println("Returning to Restaurant Admin Options...");
+                Options.showRestaurantAdminOptions(sc, restaurant);
+                break;
+
+            } else if (response.equals("2")) {
+                System.out.println("Name?");
+                String name = sc.next();
+
+                System.out.println("Job title? 'Part-time [enter]'");
+                String jobTitle = sc.next();
+
+                double hourlyWage = 0;
+                while (true) {
+                    try {
+                        System.out.println("Hourly wage?");
+                        hourlyWage = sc.nextDouble();
+
+                        if (hourlyWage <= 0) {
+                            throw new InvalidPayException("Hourly wage must be greater than 0. Please try again.");
+                        }
+                        break;
+                    } catch (InvalidPayException e) {
+                        System.out.println("Error: " + e.getMessage());
+                    } catch (Exception e) {
+                        sc.next();
+                        System.out.println("Error: Enter a number.");
+                    }
+                }
+
+                System.out.println("Phone number?");
+                String phoneNumber = sc.next();
+
+                System.out.println("Email?");
+                String email = sc.next();
+
+                Employee newPartTimeEmployee = new PartTimeEmployee(name, jobTitle, hourlyWage, phoneNumber, email);
+
+                restaurant.addEmployee(newPartTimeEmployee);
+                System.out.println("\nAdded " + newPartTimeEmployee.getName() + " to " + restaurant.getName());
+                System.out.println("\nReturning to Restaurant Admin Options...");
+                Options.showRestaurantAdminOptions(sc, restaurant);
+                break;
+
+            } else if (response.equals("3")) {
+                System.out.println("Enter employee ID to fire:");
+                int employeeId;
+                while (true) {
+                    try {
+                        employeeId = sc.nextInt();
+                        if (employeeId <= 0) {
+                            throw new InvalidIdException("ID must be greater than 0.");
+                        }
+                        boolean removed = false;
+                        for (Employee employee : restaurant.getEmployees()) {
+                            if (employee.getId() == employeeId) {
+                                restaurant.removeEmployee(employee);
+                                System.out.println("\nRemoved " + employee.getName() + ":" + employee.getId()
+                                        + " from " + restaurant.getName());
+                                removed = true;
+                                break;
+                            }
+                        }
+                        if (!removed) {
+                            throw new InvalidIdException("Employee ID: " + employeeId + " doesn't exist. Try again.");
+                        }
+                        break;
+                    } catch (InvalidIdException e) {
+                        System.out.println("Error: " + e.getMessage());
+                    } catch (Exception e) {
+                        sc.next();
+                        System.out.println("Error: Enter a number.");
+                    }
+                    System.out.println("\nReturning to Restaurant Admin Options...");
+                    Options.showRestaurantAdminOptions(sc, restaurant);
+                    break;
+                }
+            } else if (response.equals("4")) {
+                for (Employee employee : restaurant.getEmployees()) {
+                    employee.getEmployeeInfo();
+                }
+                System.out.println("Returning to Restaurant Admin Options...");
+                Options.showRestaurantAdminOptions(sc, restaurant);
+                break;
+            } else if (response.equals("5")) {
+                Options.showFullTimeOptions(sc, restaurant);
+                break;
+            } else if (response.equals("6") || response.equalsIgnoreCase("Exit")) {
+                System.out.println("Goodbye!");
+                sc.close();
+                System.exit(0);
+            } else {
+                System.out.printf("\n'%s' is not an available option. Try again.\n", response);
+                System.out.println("Pick an option (1-6):");
+                response = sc.next();
+            }
+        }
     }
 
     // inventory options
-    public static void showInventoryOptions(String response, Scanner sc) {
+    public static void showInventoryOptions(Scanner sc) {
 
     }
 
