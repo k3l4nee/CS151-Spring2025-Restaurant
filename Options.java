8dee52facd0bb5aada1bce9b68dc1345e42b9ee7
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-23 01:50:31 -0700

    added fulltime options

--- Options.java
+++ Options.java
@@ -4,7 +4,6 @@ public class Options {
 
     // welcome options
     public static void showWelcomeOptions(Scanner sc) {
-        System.out.println("Welcome!");
         System.out.println("-----------------------");
         System.out.println("Are you an employee or customer?\n");
         System.out.println(
@@ -70,18 +69,56 @@ public class Options {
 
     // full-time employee options
     public static void showFullTimeOptions(String response, Scanner sc) {
-        
+        System.out.println("-----------------------");
+        System.out.println("Full-time employee options\n");
+        System.out.println(
+                "  1. Restaurant Admin\n  2. Inventory\n  3. Back\n  4. Exit\n");
+        System.out.println("Pick an option (1-4):");
+
+        response = sc.next();
+
+        while (true) {
+            if (response.equals("1")) {
+                Options.showRestaurantAdminOptions(response, sc);
+                break;
+            } else if (response.equals("2")) {
+                Options.showInventoryOptions(response, sc);
+                break;
+            } else if (response.equals("3")) {
+                Options.showEmployeeOptions(response, sc);
+                break;
+            } else if (response.equals("4") || response.equalsIgnoreCase("Exit")) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.printf("\n'%s' is not an available option. Try again.\n", response);
+                System.out.println("Are you a full-time or part-time employee?\n");
+                System.out.println(
+                        "  1. Full-time\n  2. Part-time\n  3. Back\n  4. Exit\n");
+                System.out.println("Pick an option (1-4):");
+                response = sc.next();
+            }
+        }
     }
 
     // part-time employee options
     public static void showPartTimeOptions(String response, Scanner sc) {
-        
+
     }
 
     // customer options
     public static void showCustomerOptions(String response, Scanner sc) {
-        
+
     }
-    
+
+    // restaurant admin options
+    public static void showRestaurantAdminOptions(String response, Scanner sc) {
+
+    }
+
+    // inventory options
+    public static void showInventoryOptions(String response, Scanner sc) {
+
+    }
 
 }
