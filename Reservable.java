3d5311450f94b40090d53e3c84e476440df915e1
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-23 13:35:04 -0700

    removed getlist from reservable, added list to table

--- Reservable.java
+++ Reservable.java
@@ -1,8 +1,5 @@
-import java.util.List;
-
 interface Reservable {
     boolean makeReservation();
     boolean cancelReservation();
     boolean isAvailable();
-    List<Object> getAvailableTimeSlots();
 }
