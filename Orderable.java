0c58f89c4bbcc3c12160d9900270ec28fc5a8279
Eric Nguyen <eriicnguyen1@gmail.com>
2025-03-23 22:30:16 -0700

    updated Orderable

--- Orderable.java
+++ Orderable.java
@@ -1,9 +1,4 @@
-import java.util.List;
-
 public interface Orderable {
-    public void addToOrder(MenuItem item, int quantity);
-    public void removeFromOrder(MenuItem item, int quantity);
-    public double calculateTotal(List<MenuItem> order);
-    public void submitOrder(List<MenuItem> order);
+    public void order(MenuItem item, int quantity);
     public String getSummary();
 }
